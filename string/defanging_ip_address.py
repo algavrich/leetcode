0a5857834d15186cc76bffbcb45bc2eec5b66f09
Lucy Gavrich <annalgav@gmail.com>
2022-11-30 11:08:13 -0800

    Defanging IP address exercise

---
+++ string/defanging_ip_address.py
@@ -0,0 +1,28 @@
+"""Given a valid (IPv4) IP address, return a defanged version
+of that IP address.
+
+A defanged IP address replaces every period "." with "[.]".
+
+>>> defang("1.1.1.1")
+"1[.]1[.]1[.]1"
+
+>>> defang("255.100.50.0")
+"255[.]100[.]50[.]0"
+
+"""
+
+
+def defang(address: str) -> str:
+    """First, intuitive solution. Accepted."""
+
+    return address.replace(".", "[.]")
+
+
+def defang(address: str) -> str:
+    """Second solution, found in discussion.
+    
+    More efficient for both space and time.
+
+    """
+
+    return "[.]".join(address.split("."))
