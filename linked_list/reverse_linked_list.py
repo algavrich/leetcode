523805a03eff3b9ee044f32ae1e45a701dd3a70f
Lucy Gavrich <annalgav@gmail.com>
2023-01-10 10:25:04 -0800

    Reverse Linked List exercise

---
+++ linked_list/reverse_linked_list.py
@@ -0,0 +1,70 @@
+"""Given the head of a singly linked list, reverse the list, and return the
+reversed list.
+
+>>> linked_list = LinkedList()
+>>> linked_list.add_node_end(ListNode(1))
+>>> linked_list.add_node_end(ListNode(2))
+>>> linked_list.add_node_end(ListNode(3))
+>>> linked_list.add_node_end(ListNode(4))
+>>> linked_list.add_node_end(ListNode(5))
+>>> linked_list.reverse_list()
+[5, 4, 3, 2, 1]
+
+>>> linked_list2 = LinkedList()
+>>> linked_list2.add_node_end(ListNode(1))
+>>> linked_list2.add_node_end(ListNode(2))
+>>> linked_list2.reverse_list()
+[2, 1]
+
+>>> linked_list3 = LinkedList()
+>>> linked_list3.reverse_list()
+[]
+
+"""
+
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self) -> None:
+        self.head = None
+        self.tail = None
+
+    def add_node_end(self, node_to_add: ListNode) -> None:
+        if self.head == None and self.tail == None:
+            self.head = node_to_add
+            self.tail = node_to_add
+        elif self.head == self.tail:
+            self.head.next = node_to_add
+            self.tail = node_to_add
+        else:
+            self.tail.next = node_to_add
+            self.tail = node_to_add
+
+    def reverse_list(self) -> Optional[ListNode]:
+        """First, intuitive solution. Accepted.""" 
+        prev = None
+        curr = self.head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        ans = []
+        curr = prev
+        while curr:
+            ans.append(curr.val)
+            curr = curr.next
+        return ans
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
